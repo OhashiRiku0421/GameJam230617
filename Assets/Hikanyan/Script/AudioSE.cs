23e384476cda0aaaacc7f8cae96b2d681c18cc70
tatsupiko <identity2022@outlook.jp>
2023-06-17 19:17:40 +0900

    [Update] InGameの調整

--- Assets/Hikanyan/Script/AudioSE.cs
+++ Assets/Hikanyan/Script/AudioSE.cs
@@ -1,21 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class AudioSE : MonoBehaviour
 {
+    [SerializeField] private KeyCode _player1Se = KeyCode.None;
+    [SerializeField] private KeyCode _player2Se = KeyCode.None;
     [SerializeField] private AudioManager _audioManager;
     [SerializeField] private AudioClip _audioClip1;
     [SerializeField] private AudioClip _audioClip2;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(_player1Se))
         {
             _audioManager.PlaySoundEffect(_audioClip1);
         }
-        if (Input.GetKeyDown(KeyCode.Keypad8))
+        if (Input.GetKeyDown(_player2Se))
         {
             _audioManager.PlaySoundEffect(_audioClip2);
         }
